bd0aa4597f42edb15082bf83630fba07ea611b2f
arist <kostya.74.74@gmail.com>
2016-04-11 20:04:58 +0500

    добавил распределение по ящикам

--- coursework.py
+++ coursework.py
@@ -1,18 +1,16 @@
 import random
 
 elements = []
-containers = []
+containers = [0]
 
 for value in range(0, 10000):
     elements.append(random.random())
-elements.sort()
-
-# Write to containers
-
-for value in containers:
-    print(value)
-
-print("---------------")
+elements.sort(reverse=True)
 
 for value in elements:
-    print(value)
+    if containers[-1] + value <= 1:
+        containers[-1] += value
+    else:
+        containers.append(value)
+
+print(str(len(containers)) + " containers need")
