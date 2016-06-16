ff2e2499ccdfa6e500cb33a4c8e2ff3044192425
Kostya174 <kostya.74.74@gmail.com>
2016-06-16 22:30:47 +0500

    BFD на шарпе

---
+++ CourseWork/CourseWork/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CourseWork
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var containersCount = 0;
+            Console.WriteLine("Введите кол-во контейнеров");
+            try
+            {
+                containersCount = int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Неверно задано число");
+                containersCount = int.Parse(Console.ReadLine());
+            }
+            var elements = new List<double>();
+            var containers = new List<double>();
+            var rnd = new Random();
+            for (var i = 0; i < 10000; i++)
+            {
+                elements.Add(rnd.NextDouble());
+            }
+            foreach (var element in elements)
+            {
+                 var max = containers.FindAll(m => m + element <= 1).ToArray();
+
+                if (max.Length > 0)
+                {
+                    containers[containers.FindIndex(x => Equals(x, max.Max()))] += element;
+                }
+                else
+                {
+                    containers.Add(element);
+                }
+            }
+            Console.WriteLine(containers.Count <= containersCount ? containers.Count.ToString() : "Недостаточно контейнеров");
+        }
+    }
+}
---
+++ CourseWork/CourseWork/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+
+[assembly: AssemblyTitle("CourseWork")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CourseWork")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+
+[assembly: Guid("c87f25f9-6737-4d7c-887e-77c70feff9fc")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
